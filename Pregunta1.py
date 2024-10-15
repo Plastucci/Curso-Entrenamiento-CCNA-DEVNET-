6203495784a1637a6faee4768dc68a316a75ce98
NetAcad DEVASC <changeme@example.com>
2024-10-15 23:29:23 +0000

    Script Pregunta 1

---
+++ Pregunta1.py
@@ -0,0 +1,12 @@
+# Solicitar datos al usuario
+nombre = input("Por favor, ingresa tu nombre: ")
+apellido = input("Por favor, ingresa tu apellido: ")
+edad = input("Por favor, ingresa tu edad: ")
+sede = input("Por favor, ingresa tu sede: ")
+
+# Imprimir los datos ingresados
+print("\nLos datos ingresados son:")
+print(f"Nombre: {nombre}")
+print(f"Apellido: {apellido}")
+print(f"Edad: {edad}")
+print(f"Sede: {sede}")
